02e8400a6fc069718b0686e6f7b90c98640d6a01
manoj98gopal <manoj98gopal@gmail.com>
2026-03-20 10:44:39 +0530

    decorators completed

---
+++ 04_generics/08_exerices.ts
@@ -0,0 +1,15 @@
+// generic function
+function echo<T>(arg: T): T {
+  return arg;
+}
+
+function printName<T extends { name: string }>(obj: T) {
+  console.log(obj.name);
+}
+
+class Entity<T> {
+  constructor(public id: T) {}
+}
+
+// Given the following interface, what does keyof User return?
+// It returns a union of the properties of User: ‘userId’ | ‘username’
