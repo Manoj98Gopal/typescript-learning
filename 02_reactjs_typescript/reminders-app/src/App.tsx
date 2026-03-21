550ad7ed88ea3e7f8c76d922063aa45141e293bf
manoj98gopal <manoj98gopal@gmail.com>
2026-03-21 14:58:58 +0530

    typescript react js project created and tailwind css added

---
+++ 02_reactjs_typescript/reminders-app/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-6">
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-blue-600">
+        Click me{" "}
+      </button>
+    </div>
+  );
+}
+
+export default App;
